82fd7bd6ff94b78c0feec4a7c360e97d3e3a5e60
Robert Good <robert.good@goodtocode.com>
2024-06-29 12:04:10 -0700

    added controller methods for getauthorchat

--- src/Presentation/WebApi/Authors/AuthorsController.cs
+++ src/Presentation/WebApi/Authors/AuthorsController.cs
@@ -1,4 +1,5 @@
 ﻿using Goodtocode.SemanticKernel.Core.Application.Author;
+using Goodtocode.SemanticKernel.Core.Application.ChatCompletion;
 using Goodtocode.SemanticKernel.Core.Application.Common.Models;
 using Goodtocode.SemanticKernel.Presentation.WebApi.Common;
 
@@ -38,6 +39,76 @@ public class AuthorController : ApiControllerBase
         {
             AuthorId = id
         });
+    }
+
+    /// <summary>Get All Author Chat Sessions Query</summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     "AuthorId": 60fb5e99-3a78-43df-a512-7d8ff498499e
+    ///     "StartDate": "2024-06-01T00:00:00Z"
+    ///     "EndDate": "2024-12-01T00:00:00Z"
+    ///     "api-version":  1.0
+    /// 
+    /// </remarks>
+    /// <returns>
+    /// ChatSessionDto
+    ///     { Id: 1efb5e99-3a78-43df-a512-7d8ff498499e
+    ///     AuthorId: 4dfb5e99-3a78-43df-a512-7d8ff498499e
+    ///     Timestamp: "2024-06-03T11:21:00Z"
+    ///     Messages: [
+    ///         {
+    ///             "Id": 60fb5e99-3a78-43df-a512-7d8ff498499e,
+    ///             "Content": "Certainly! Semantic Kernel is a great framework for AI.",
+    ///         }
+    ///     }]
+    /// </returns>
+    [HttpGet("{id}/ChatSessions", Name = "GetAuthorChatSessionsQuery")]
+    [ProducesResponseType(typeof(ICollection<ChatSessionDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ICollection<ChatSessionDto>> GetAuthorChatSessionsQuery(Guid id)
+    {
+        return await Mediator.Send(new GetAuthorChatSessionsQuery() { AuthorId = id });
+    }
+
+    /// <summary>Get Author Chat Sessions Paginated Query</summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     "StartDate": "2024-06-01T00:00:00Z"
+    ///     "EndDate": "2024-12-01T00:00:00Z"
+    ///     "PageNumber": 1
+    ///     "PageSize" : 10
+    ///     "api-version":  1.0
+    /// 
+    /// </remarks>
+    /// <returns>
+    /// ChatSessionDto
+    ///     { Id: 1efb5e99-3a78-43df-a512-7d8ff498499e
+    ///     AuthorId: 4dfb5e99-3a78-43df-a512-7d8ff498499e
+    ///     Messages: [
+    ///         {
+    ///             "Id": 60fb5e99-3a78-43df-a512-7d8ff498499e,
+    ///             "Content": "Certainly! Semantic Kernel is a great framework for AI.",
+    ///         }
+    ///     }]
+    /// </returns>
+    [HttpGet("{id}/ChatSessions/Paginated", Name = "GetAuthorChatSessionsPaginatedQuery")]
+    [ProducesResponseType(typeof(PaginatedList<ChatSessionDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<PaginatedList<ChatSessionDto>>> GetAuthorChatSessionsPaginatedQuery(Guid id, DateTime? startDate, DateTime? endDate, int pageNumber = 1, int pageSize = 10)
+    {
+        var query = new GetAuthorChatSessionsPaginatedQuery()
+        {
+            AuthorId = id,
+            StartDate = startDate,
+            EndDate = endDate,
+            PageNumber = pageNumber,
+            PageSize = pageSize
+
+        };
+        return await Mediator.Send(query);
     }
 
     /// <summary>
