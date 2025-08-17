5d6d6eac5aa2b4c0b99ed590dce9968d79718270
Robert Good <robert.good@goodtocode.com>
2025-08-16 19:46:31 -0700

    Upgraded dependencies

--- src/Infrastructure.SqlServer/Migrations/20250816225410_v1.1.1.Designer.cs
+++ src/Infrastructure.SqlServer/Migrations/20250816225410_v1.1.1.Designer.cs
@@ -4,6 +4,7 @@ using Goodtocode.SemanticKernel.Infrastructure.SqlServer.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Goodtocode.SemanticKernel.Infrastructure.SqlServer.Migrations
 {
     [DbContext(typeof(SemanticKernelContext))]
-    partial class SemanticKernelContextModelSnapshot : ModelSnapshot
+    [Migration("20250816225410_v1.1.1")]
+    partial class v111
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure.SqlServer/Migrations/20250816225410_v1.1.1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Goodtocode.SemanticKernel.Infrastructure.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class v111 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "ChatSessions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "NVARCHAR(200)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(200)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "ChatSessions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "NVARCHAR(200)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(200)",
+                oldNullable: true);
+        }
+    }
+}
